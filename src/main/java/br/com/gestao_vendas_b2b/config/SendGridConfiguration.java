1e5179bf39b3ee19390bffd4274ada448121e05e
educoelho03 <eduardopaivacoelho@outlook.com>
2025-09-03 01:47:54 -0300

    add: implementing new features to send emails automatically

---
+++ src/main/java/br/com/gestao_vendas_b2b/config/SendGridConfiguration.java
@@ -0,0 +1,31 @@
+package br.com.gestao_vendas_b2b.config;
+
+import br.com.gestao_vendas_b2b.model.entities.SendGridProperties;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.objects.Email;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(SendGridConfiguration.class)
+public class SendGridConfiguration {
+
+    private final SendGridProperties sendGridProperties;
+
+    public SendGridConfiguration(SendGridProperties sendGridProperties) {
+        this.sendGridProperties = sendGridProperties;
+    }
+
+    @Bean
+    public SendGrid sendGrid(){
+        String apiKey = sendGridProperties.getApiKey();
+        return new SendGrid(apiKey);
+    }
+
+    @Bean
+    public Email fromEmail() {
+        String fromEmail = sendGridProperties.getSenderEmail();
+        return new Email(fromEmail);
+    }
+}
--- src/main/java/br/com/gestao_vendas_b2b/service/EmailService.java
+++ src/main/java/br/com/gestao_vendas_b2b/service/EmailService.java
@@ -1,7 +1,44 @@
 package br.com.gestao_vendas_b2b.service;
 
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class EmailService {
+
+    private static final String EMAIL_ENDPOINT = "mail/send";
+
+    private final SendGrid sendGrid;
+    private final Email fromEmail;
+
+    public EmailService(SendGrid sendGrid, Email fromEmail) {
+        this.sendGrid = sendGrid;
+        this.fromEmail = fromEmail;
+    }
+
+    public void sendMail(String to, String subject, String body){
+        Email toEmail = new Email(to);
+        Content content = new Content("text/plain", body);
+        Mail mail = new Mail(fromEmail, subject, toEmail, content);
+
+        try {
+            Request request = new Request();
+            request.setMethod(Method.POST);
+            request.setEndpoint(EMAIL_ENDPOINT);
+            request.setBody(mail.build());
+
+            sendGrid.api(request);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Erro no envio de emails: " + e);
+        }
+
+    }
 }
